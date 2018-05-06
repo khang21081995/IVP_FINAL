0ae7d956d26c2f419035f7edb93f56ffa1a3126c
Phạm Quang Khang <khangpq@Phams-MacBook-Pro.local>
2018-05-07 01:13:15 +0700

    khangPQ INIT

---
+++ demo.js
@@ -0,0 +1,31 @@
+var Tesseract = require('tesseract.js');
+var cv = require('opencv4nodejs');
+var myImage = "./image2.png";
+
+
+var img = cv.imread(myImage)
+// gray scaling
+img = img.bgrToGray();
+
+var kernel = new cv.Mat(1, 1, 1, 1);
+img = img.dilate(kernel)
+img = img.erode(kernel)
+cv.imwrite(__dirname + "/image_after_filter_noise.png", img)
+
+// img = img.adaptiveThreshold(255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 31, 2)
+
+
+cv.imwrite(__dirname + "/image_final.png", img)
+
+Tesseract.recognize("./image_final.png")
+    .progress(function (p) {
+        console.log('progress', p)
+    }).catch(err => console.log(err))
+    .then(function (result) {
+        console.error('\n\nresult: ', result.text)
+        console.error('confidence: ', result.confidence + "%")
+        // console.error(result)
+    })
+
+
+//result: tesseraét.proce55(_dirhame i- '7image.jbg',function(err, text)
---
+++ readme.md
@@ -0,0 +1,11 @@
+##1. Cài đặt home brew: Coppy parse vào termin
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+##2. Cài đặt Cmake
+brew install cmake
+##3. cài đặt nodejs
+##4. cd vào thư mục mới down về 
+gõ `npm i` nếu báo lỗi thì chạy câu bên dưới rồi quay lại chạy lại npm i
+sudo xcodebuild -license 
+
+##5. sau khi run xong chạy `node demo.js`
+
